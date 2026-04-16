1610db088d53843e9e404d8df993f180e74369b5
Giovanne Bianchi <giovannebianchisilva@gmail.com>
2026-04-16 20:31:33 -0300

    feat: regressao_mais_tipos

--- regressao.py
+++ regressao.py
@@ -1,6 +1,6 @@
 #%%
 import pandas as pd
-from sklearn import linear_model
+from sklearn import linear_model, tree
 import matplotlib.pyplot as plt
 
 df = pd.read_excel("data/dados_cerveja_nota.xlsx")
@@ -14,13 +14,21 @@ y = df['nota']
 reg = linear_model.LinearRegression()
 reg.fit(X, y)
 
+arvore_full = tree.DecisionTreeRegressor(random_state=42)
+arvore_full.fit(X, y)
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+arvore_d2 = tree.DecisionTreeRegressor(random_state=42, max_depth=2)
+arvore_d2.fit(X, y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
+
 #%%
 # Coeficientes
 a, b = reg.intercept_, reg.coef_[0]
 print(a, b)
 
 #%%
-predict = reg.predict(X.drop_duplicates())
+predict_reg = reg.predict(X.drop_duplicates())
 
 #%%
 plt.figure(dpi=400)
@@ -29,7 +37,11 @@ plt.grid(True)
 plt.title("Relação Cerveaja x Nota")
 plt.xlabel("Cerveja")
 plt.ylabel("Nota")
-plt.plot(X.drop_duplicates()['cerveja'], predict)
+plt.plot(X.drop_duplicates()['cerveja'], predict_reg)
+plt.plot(X.drop_duplicates()['cerveja'], predict_arvore_full)
+plt.plot(X.drop_duplicates()['cerveja'], predict_arvore_d2)
 plt.legend(['Observado',
             f'y = {a:.3f} + {b:.3f} x',
+            'Árvore Full',
+            'Árvore D2',
             ])
